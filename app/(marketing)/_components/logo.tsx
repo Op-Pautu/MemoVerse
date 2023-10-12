61795f7a55b9d2f3a745ff4add8b6c9ef80b9373
Op-Pautu <pautuop@gmail.com>
2023-10-12 14:15:27 +0530

    Add heroes and footer components

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,19 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" alt="logo" height="40" width="40" />
+      <p className={cn("font-semibold", font.className)}>MemoVerse</p>
+    </div>
+  );
+};
+
+export default Logo;
