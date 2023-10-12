90e0f3e6299de66894c741b4215c9baa68fbf691
Op-Pautu <pautuop@gmail.com>
2023-10-12 16:11:05 +0530

    Add convex for backend and clerk for auth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import React from "react";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
-
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  console.log(isAuthenticated);
   return (
     <div
       className={cn(
@@ -18,6 +24,27 @@ const Navbar = () => {
     >
       <Logo />
       <div className="flex md:ml-auto md:justify-end justify-between w-full items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
